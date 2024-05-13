636e581088098484a398f41c7a68d2e35bd55ee1
Kylejeong2 <Kylejeong21@gmail.com>
2024-05-12 19:55:03 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portfolio-V3",
+  build: { chunkSizeWarningLimit: 1600, },
   plugins: [react()],
 })
